eb8abf80c403e6f9a58ac5b3ca594e1f91d97eca
jimlemoine <jlemoine007@gmail.com>
2021-11-01 13:21:49 -0700

    files wired and getAll working

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,19 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config.js');
+
+const getAll = async () => {
+  const result = await db('accounts')
+  return result
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async id => {
+  const result = await db('accounts').where('id', id).first()
+  return result
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const [id] = await db('accounts').insert(account)
+  const newAccount = await getById(id)
+  return newAccount
 }
 
 const updateById = (id, account) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Account.getAll()
+    res.json(data)
+  } catch (err) {
+    next (err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
@@ -21,7 +27,10 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
